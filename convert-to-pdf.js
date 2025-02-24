37aaf53023e27bc265f32b8a439da9c8f86f1ec6
Michel MAIER <michel.maier@gmail.com>
2025-02-24 21:29:42 +0100

    feat: generate english resume

--- convert-to-pdf.js
+++ convert-to-pdf.js
@@ -1,13 +1,21 @@
 const puppeteer = require('puppeteer');
+const path = require('path');
 
 (async () => {
+  const inputPath = process.argv[2] || 'resume.html';
+  const absoluteInputPath = path.isAbsolute(inputPath)
+    ? inputPath
+    : path.join(process.cwd(), inputPath);
+
+  const outputPath = process.argv[3] || 'resume.pdf';
+
   const browser = await puppeteer.launch({
     args: ['--no-sandbox', '--disable-setuid-sandbox']
   });
   const page = await browser.newPage();
-  await page.goto(`file://${process.cwd()}/resume.html`, { waitUntil: 'networkidle0' });
+  await page.goto(`file://${absoluteInputPath}`, { waitUntil: 'networkidle0' });
   await page.pdf({
-    path: 'resume_michelmaier_fr.pdf',
+    path: outputPath,
     format: 'A4',
     printBackground: true,
     margin: { top: '0.5in', right: '0.5in', bottom: '0.5in', left: '0.5in' }
---
+++ theme-english/index.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const Handlebars = require('handlebars');
+
+module.exports = {
+  render: function (resume) {
+    const css = fs.readFileSync(__dirname + '/style.css', 'utf-8');
+    const tpl = fs.readFileSync(__dirname + '/resume.hbs', 'utf-8');
+
+    Handlebars.registerHelper('formatDate', function(dateString) {
+      if (!dateString) return '';
+      const s = dateString.split('-');
+      return `${s[1]}/${s[0]}`; // MM/YYYY
+    });
+
+    Handlebars.registerHelper('limit', function(array, limit) {
+      if (Array.isArray(array)) {
+        return array.slice(0, limit);
+      }
+      return array;
+    });
+
+    Handlebars.registerHelper('ifMoreThan', function(array, limit, options) {
+      if (Array.isArray(array) && array.length > limit) {
+        return options.fn(this);
+      }
+      return '';
+    });
+
+    return Handlebars.compile(tpl)({
+      css: css,
+      resume: resume
+    });
+  }
+};
